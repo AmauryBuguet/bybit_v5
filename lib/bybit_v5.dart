2d6c756d31f631d4920bd7191c1c67925a9ff46d
AmauryBuguet <amo.buguet@gmail.com>
2024-06-05 17:26:07 +0200

    fixed missing exports

--- lib/bybit_v5.dart
+++ lib/bybit_v5.dart
@@ -4,14 +4,40 @@
 library bybit_v5;
 
 export 'src/bybit_api.dart';
+export 'src/models/classes/cancel_all_response.dart';
+export 'src/models/classes/closed_pnl_record.dart';
+export 'src/models/classes/closed_pnl_response.dart';
+export 'src/models/classes/coin_balance.dart';
+export 'src/models/classes/fee_rate.dart';
+export 'src/models/classes/instrument_info.dart';
+export 'src/models/classes/instruments_response.dart';
 export 'src/models/classes/kline.dart';
+export 'src/models/classes/order.dart';
+export 'src/models/classes/order_ids.dart';
+export 'src/models/classes/orders_list_response.dart';
+export 'src/models/classes/position.dart';
+export 'src/models/classes/position_list_response.dart';
+export 'src/models/classes/subscription.dart';
+export 'src/models/classes/ticker_info.dart';
 export 'src/models/classes/trade.dart';
+export 'src/models/classes/trade_list_response.dart';
+export 'src/models/classes/user_trade.dart';
+export 'src/models/classes/wallet_balance.dart';
+export 'src/models/classes/ws_kline.dart';
+export 'src/models/classes/ws_order.dart';
+export 'src/models/classes/ws_orderbook.dart';
+export 'src/models/classes/ws_position.dart';
+export 'src/models/classes/ws_trade.dart';
+export 'src/models/classes/ws_wallet.dart';
+export 'src/models/enums/account_type.dart';
 export 'src/models/enums/category.dart';
+export 'src/models/enums/instrument_status.dart';
 export 'src/models/enums/market_unit.dart';
 export 'src/models/enums/option_type.dart';
 export 'src/models/enums/order_filter.dart';
 export 'src/models/enums/order_status.dart';
 export 'src/models/enums/order_type.dart';
+export 'src/models/enums/orderbook_depth.dart';
 export 'src/models/enums/position_idx.dart';
 export 'src/models/enums/side.dart';
 export 'src/models/enums/stop_order_type.dart';
@@ -20,5 +46,3 @@ export 'src/models/enums/time_interval.dart';
 export 'src/models/enums/tpsl_mode.dart';
 export 'src/models/enums/trigger_by.dart';
 export 'src/models/enums/trigger_direction.dart';
-
-// TODO: Export any libraries intended for clients of this package.
