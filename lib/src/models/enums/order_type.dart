9a6ad890bb285eeede4f3f347b6ad8192d31d1d8
AmauryBuguet <amo.buguet@gmail.com>
2024-06-08 16:35:48 +0200

    removed unknown order type

--- lib/src/models/enums/order_type.dart
+++ lib/src/models/enums/order_type.dart
@@ -1,10 +1,11 @@
+import 'package:collection/collection.dart';
+
 /// Enumeration of supported order types for queries to the Bybit API.
 ///
 /// For more information, refer to the [Bybit API documentation](https://bybit-exchange.github.io/docs/v5/enum#ordertype).
 enum OrderType {
   market("Market"),
-  limit("Limit"),
-  unknown("UNKNOWN");
+  limit("Limit");
 
   final String json;
 
@@ -13,5 +14,10 @@ enum OrderType {
   /// Allows to get a [OrderType] object from API responses
   factory OrderType.fromString(String str) {
     return OrderType.values.singleWhere((e) => e.json == str);
+  }
+
+  /// Allows to get a [OrderType] object from API responses where it might not be
+  static OrderType? tryFromString(String? str) {
+    return OrderType.values.singleWhereOrNull((e) => e.json == str);
   }
 }
